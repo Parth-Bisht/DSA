423a155289db35bd7961cbeb4ac9f9ae8766739f
Parth Bisht <bishtparth795@gmail.com>
2023-04-01 15:33:19 +0530

    sqrt(x)

---
+++ Sqrt(x)/Main.java
@@ -0,0 +1,15 @@
+/*
+Given a non-negative integer x, return the square root of x rounded down to the nearest integer.
+The returned integer should be non-negative as well.
+
+Input: x = 8
+Output: 2
+Explanation: The square root of 8 is 2.82842...,
+and since we round it down to the nearest integer, 2 is returned.
+ */
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
