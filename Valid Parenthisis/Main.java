a1524fbe032f052fb55626f72b65a702289e06ad
Parth Bisht <bishtparth795@gmail.com>
2023-04-01 13:44:48 +0530

    valid parenthesis

---
+++ Valid Parenthisis/Main.java
@@ -0,0 +1,14 @@
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<Character>();
+        for(char c: s.toCharArray()){
+            if(c=='(')
+        }
+    }
+}
