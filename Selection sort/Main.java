162e31b8d3b6cdb353382425518f67241118f5db
Parth Bisht <bishtparth795@gmail.com>
2023-04-03 12:53:59 +0530

    selection sort implementation

--- Selection sort/Main.java
+++ Selection sort/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /*
     Selection sort
  */
@@ -6,7 +8,7 @@ public class Main {
         int[] arr = {64,25,12,22,11};
         SelectionSort output = new SelectionSort(arr);
         int[] sortedArray = output.sort();
-        System.out
+        System.out.println(Arrays.toString(sortedArray));
 //        for(int e: sortedArray){
 //            System.out.println(e);
 //        }
