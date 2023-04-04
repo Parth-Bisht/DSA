f274449ce2031bf725e94323562315a44b26dcf5
Parth Bisht <bishtparth795@gmail.com>
2023-04-04 11:27:54 +0530

    bubble sort

---
+++ Sorting algos/BubbleSort.java
@@ -0,0 +1,25 @@
+public class BubbleSort {
+    private int[] arr;
+
+    public BubbleSort(int[] arr) {
+        this.arr = arr;
+    }
+
+    public int[] sort(){
+        int n = arr.length;
+        for(int i=0;i<n-1;i++){
+            for(int j=0;j<n-i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    swap(arr,j,j+1);
+                }
+            }
+        }
+        return arr;
+    }
+
+    public static void swap(int arr[],int i,int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
--- Sorting algos/Main.java
+++ Sorting algos/Main.java
@@ -6,11 +6,15 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         int[] arr = {64,25,12,22,11};
-        SelectionSort output = new SelectionSort(arr);
-        int[] sortedArray = output.sort();
-        System.out.println(Arrays.toString(sortedArray));
+//        SelectionSort output = new SelectionSort(arr);
+//        int[] sortedArray = output.sort();
+//        System.out.println(Arrays.toString(sortedArray));
 //        for(int e: sortedArray){
 //            System.out.println(e);
 //        }
+
+        BubbleSort output = new BubbleSort(arr);
+        int[] sortedArray = output.sort();
+        System.out.println(Arrays.toString(sortedArray));
     }
 }
