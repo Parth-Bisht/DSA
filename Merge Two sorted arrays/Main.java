c871ea1d2d345bcdda94b6c2a8ebae9c0c24c859
Parth Bisht <bishtparth795@gmail.com>
2023-04-06 18:17:35 +0530

    merge sorted arrays

--- Merge Two sorted arrays/Main.java
+++ Merge Two sorted arrays/Main.java
@@ -16,22 +16,24 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        int[] nums1 = {1,2,3};
+        int[] nums1 = {1,2,3,0,0,0};
         int[] nums2 = {2,5,6};
         int m=3, n = 3;
-        System.out.println(merge(nums1,m,nums2,n));
+        merge(nums1, m, nums2, n);
+        System.out.println(Arrays.toString(nums1));
     }
 
-    public static List<Integer> merge(int[] nums1, int m, int[] nums2, int n){
-        List<Integer> temp = new ArrayList<Integer>();
-        int i = 0;
-        int j = 0;
-        while(i<m || j<n){
-            if(nums1[i]<=nums2[j]) temp.add(nums1[i++]);
-            else temp.add(nums2[j++]);
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m-1;
+        int j = n-1;
+        int k = m+n-1;
+        while(j>=0){
+            if(i>=0 && nums1[i]>nums2[j]){
+                nums1[k--]=nums1[i--];
+            }else{
+                nums1[k--]=nums2[j--];
+            }
         }
-        while(i<m) temp.add(nums1[i++]);
-        while(j<n) temp.add(nums2[j++]);
-        return temp;
     }
+
 }
