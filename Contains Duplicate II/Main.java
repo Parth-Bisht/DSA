5bada2aef6be52af09c7b284c9d0310ee65a05ad
Parth Bisht <bishtparth795@gmail.com>
2023-04-10 11:45:54 +0530

    contains duplicate

--- Contains Duplicate II/Main.java
+++ Contains Duplicate II/Main.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 /*
 Given an integer array nums and an integer k,
 return true if there are two distinct indices i and j in the array such that
@@ -12,5 +14,15 @@ Output: false
 public class Main {
     public static void main(String[] args) {
 
+    }
+    public static boolean containsNearbyDuplicate(int[] nums, int k){
+        HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
+
+        for(int i=0;i<nums.length;i++){
+            if(map.containsKey(nums[i])){
+                
+            }
+            map.put(nums[i],i);
+        }
     }
 }
