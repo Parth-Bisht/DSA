cd477159741e86a1d44d124bbe7ec9ca3582a1ee
Parth Bisht <bishtparth795@gmail.com>
2023-04-10 11:42:56 +0530

    contains duplicate II

---
+++ Contains Duplicate II/Main.java
@@ -0,0 +1,16 @@
+/*
+Given an integer array nums and an integer k,
+return true if there are two distinct indices i and j in the array such that
+nums[i] == nums[j] and abs(i - j) <= k.
+
+Input: nums = [1,2,3,1], k = 3
+Output: true
+
+Input: nums = [1,2,3,1,2,3], k = 2
+Output: false
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
