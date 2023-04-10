7eae0ee677ae2a3fd2c8c0057f62763596f42ebd
Parth Bisht <bishtparth795@gmail.com>
2023-04-10 11:47:48 +0530

    contains duplicate

--- Contains Duplicate II/Main.java
+++ Contains Duplicate II/Main.java
@@ -13,16 +13,21 @@ Output: false
  */
 public class Main {
     public static void main(String[] args) {
-
+        int[] nums = {1,2,3,1,2,3};
+        int k = 2;
+        System.out.println(containsNearbyDuplicate(nums,k));
     }
     public static boolean containsNearbyDuplicate(int[] nums, int k){
         HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
 
         for(int i=0;i<nums.length;i++){
             if(map.containsKey(nums[i])){
-                
+                int pre = map.get(nums[i]);
+                if(i-pre<=k) return true;
             }
             map.put(nums[i],i);
         }
+
+        return false;
     }
 }
