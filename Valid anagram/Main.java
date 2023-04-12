7a05814e744a95e538ae12c8d03ce44c1c3030ea
Parth Bisht <bishtparth795@gmail.com>
2023-04-12 19:48:43 +0530

    valid anargam

--- Valid anagram/Main.java
+++ Valid anagram/Main.java
@@ -12,10 +12,22 @@ Output: false
 
 public class Main {
     public static void main(String[] args) {
-
+        String s = "anargam";
+        String t = "nagaram";
+        System.out.println(isAnargam(s,t));
     }
 
     public static boolean isAnargam(String s,String t){
+        if(s.length()!=t.length()) return false;
+        int[] freq1 = new int[26];
+        int[] freq2 = new int[26];
+        for(int i=0;i<s.length();i++){
+            freq1[s.charAt(i)-'a']++;
+            freq2[t.charAt(i)-'a']++;
+        }
+        for(int i=0;i<26;i++){
+            if(freq1[i]!=freq2[i]) return false;
+        }
         return true;
     }
 }
