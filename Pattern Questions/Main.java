ccff48139cadc766b0c2ff12f1f6f36f57465ef8
Parth Bisht <bishtparth795@gmail.com>
2023-04-15 23:04:05 +0530

    simple pattern

--- Pattern Questions/Main.java
+++ Pattern Questions/Main.java
@@ -1,11 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-
+        simplePattern(5);
     }
 
-    public static void sinplePattern(int n){
-
+    public static void simplePattern(int n){
+        for(int i=0;i<n;i++){
+            String bag = "";
+            for(int j=0;j<n;j++){
+                bag += "* ";
+            }
+            System.out.println(bag);
+        }
     }
+
+    public static void
 }
 
 
