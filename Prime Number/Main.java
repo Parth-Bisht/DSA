6768afbb6141ed4e22e94a3adcb0a5bf3085e380
Parth Bisht <bishtparth795@gmail.com>
2023-04-15 22:57:23 +0530

    prime number

--- Prime Number/Main.java
+++ Prime Number/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-
+        int num = 7;
+        System.out.println(isPrime(num));
     }
     public static boolean isPrime(int n){
         for(int i=2;i<n;i++){
