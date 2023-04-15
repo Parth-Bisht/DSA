bbb8708757b5541ac229cc5a3e2ae38ce3cbb713
Parth Bisht <bishtparth795@gmail.com>
2023-04-15 22:52:20 +0530

    Prime number

--- Prime Number/Main.java
+++ Prime Number/Main.java
@@ -3,6 +3,9 @@ public class Main {
 
     }
     public static boolean isPrime(int n){
-        
+        for(int i=2;i<n;i++){
+            if(n%i==0) return false;
+        }
+        return true;
     }
 }
