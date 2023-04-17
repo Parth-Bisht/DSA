3c072ac71978dfea175439b5fa5b713ccdee6c14
Parth Bisht <bishtparth795@gmail.com>
2023-04-17 12:28:36 +0530

    Majority element

--- Majority Element/Main.java
+++ Majority Element/Main.java
@@ -10,8 +10,20 @@ Input: nums = [2,2,1,1,1,2,2]
 Output: 2
  */
 
+import java.util.HashMap;
+
 public class Main {
     public static void main(String[] args) {
+        int[] nums = {2,2,1,1,1,2,2};
+        System.out.println(majorityElement(nums));
+    }
 
+    public static int majorityElement(int[] nums){
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+            if(map.get(nums[i])>nums.length/2) return nums[i];
+        }
+        return -1;
     }
 }
