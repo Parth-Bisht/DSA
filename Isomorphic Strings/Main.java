ed4c78226d7a3fd10af97836d1939559736a796f
Parth Bisht <bishtparth795@gmail.com>
2023-04-19 20:28:53 +0530

    Isomorphic solution

--- Isomorphic Strings/Main.java
+++ Isomorphic Strings/Main.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 /*
 Given two strings s and t, determine if they are isomorphic.
 
@@ -20,7 +22,19 @@ public class Main {
 
     }
 
-    public static boolean isIsomorphic(String s, String s){
-        
+    public static boolean isIsomorphic(String s, String t){
+        HashMap <Character,Character> replacement = new HashMap<>();
+        HashMap <Character, Boolean> used = new HashMap<>();
+
+        for(int i=0;i<s.length();i++) {
+            if (replacement.containsKey((s.charAt(i)))) {
+                if (replacement.get(s.charAt(i)) != t.charAt(i)) return false;
+            } else {
+                if (used.containsKey(t.charAt(i))) return false;
+                replacement.put(s.charAt(i), t.charAt(i));
+                used.put(t.charAt(i), true);
+            }
+        }
+        return true;
     }
 }
