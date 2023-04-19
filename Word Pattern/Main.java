b354dff85ec8a59e4d84d1d48803443e9faf43c8
Parth Bisht <bishtparth795@gmail.com>
2023-04-19 20:03:32 +0530

    Word Pattern

--- Word Pattern/Main.java
+++ Word Pattern/Main.java
@@ -13,10 +13,24 @@ Output: false
  */
 public class Main {
     public static void main(String[] args) {
-
+        String pattern = "abba";
+        String s = "dog cat cat dog";
+        System.out.println(wordPattern(pattern,s));
     }
 
     public static boolean wordPattern(String pattern, String s){
         HashMap<Character,String> map = new HashMap<>();
+        String words[] = s.split(" ");
+        if(pattern.length()!=words.length) return false;
+        for(int i=0;i<words.length;i++){
+            char c = pattern.charAt(i);
+            if(map.containsKey(c)){
+                if(!words[i].equals(map.get(c))) return false;
+            }else{
+                if(map.containsValue(words[i])) return false;
+                map.put(c,words[i]);
+            }
+        }
+        return true;
     }
 }
