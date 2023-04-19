490db39841405c3c7eab5606965230718732a411
Parth Bisht <bishtparth795@gmail.com>
2023-04-19 19:57:53 +0530

    Word Pattern

--- Word Pattern/Main.java
+++ Word Pattern/Main.java
@@ -1,5 +1,22 @@
+import java.util.HashMap;
+
+/*
+Given a pattern and a string s, find if s follows the same pattern.
+
+Here follow means a full match, such that there is a bijection between a letter in pattern and a non-empty word in s.
+
+Input: pattern = "abba", s = "dog cat cat dog"
+Output: true
+
+Input: pattern = "abba", s = "dog cat cat fish"
+Output: false
+ */
 public class Main {
     public static void main(String[] args) {
-        
+
+    }
+
+    public static boolean wordPattern(String pattern, String s){
+        HashMap<Character,String> map = new HashMap<>();
     }
 }
