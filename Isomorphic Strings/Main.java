d9e170fd08bcde368cbe9ad7b9003764ecbb0d17
Parth Bisht <bishtparth795@gmail.com>
2023-04-19 20:25:48 +0530

    Isomorphic String

--- Isomorphic Strings/Main.java
+++ Isomorphic Strings/Main.java
@@ -1,5 +1,26 @@
+/*
+Given two strings s and t, determine if they are isomorphic.
+
+Two strings s and t are isomorphic if the characters in s can be replaced to get t.
+
+All occurrences of a character must be replaced with another character while preserving the order of characters.
+No two characters may map to the same character, but a character may map to itself.
+
+Input: s = "egg", t = "add"
+Output: true
+
+Input: s = "foo", t = "bar"
+Output: false
+
+Input: s = "paper", t = "title"
+Output: true
+ */
 public class Main {
     public static void main(String[] args) {
+
+    }
+
+    public static boolean isIsomorphic(String s, String s){
         
     }
 }
