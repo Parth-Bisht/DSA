f3a52c773b1bd4e27b7594fc6d812a25ca495574
Parth Bisht <bishtparth795@gmail.com>
2023-04-19 19:40:29 +0530

    arrange coins

--- Arranging Coins/Main.java
+++ Arranging Coins/Main.java
@@ -1,5 +1,19 @@
+/*
+You have n coins and you want to build a staircase with these coins. The staircase consists of k rows where the ith row has exactly i coins. The last row of the staircase may be incomplete.
+
+Given the integer n, return the number of complete rows of the staircase you will build.
+ */
+
 public class Main {
     public static void main(String[] args) {
         
+    }
+    public static int arrangeCoins(int n){
+        int ans = 1;
+        while(n>0){
+            ans++;
+            n -= ans;
+        }
+        return ans-1;
     }
 }
