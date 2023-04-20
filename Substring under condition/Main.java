29f410216ed68abfaea8883ee71941b3698ef50b
Parth Bisht <bishtparth795@gmail.com>
2023-04-20 16:47:04 +0530

    substring under condition

--- Substring under condition/Main.java
+++ Substring under condition/Main.java
@@ -9,9 +9,17 @@ Substrings of abcab are - a,abca,b,bcab,c,a,b
 
 public class Main {
     public static void main(String[] args) {
-
+        String s = "zebyotzpuorypvetkvpip";
+        System.out.println(substring(s));
     }
     public static int substring(String s){
-
+        int count = 0;
+        for(int i=0;i<s.length();i++){
+            char c = s.charAt(i);
+            for(int j=i;j<s.length();j++){
+                if(s.charAt(j)==c) count++;
+            }
+        }
+        return count;
     }
 }
