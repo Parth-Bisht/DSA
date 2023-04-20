3d3e7ea520da989de757cc4b45d1d1b8905060cb
Parth Bisht <bishtparth795@gmail.com>
2023-04-20 16:41:09 +0530

    substring

--- Substring under condition/Main.java
+++ Substring under condition/Main.java
@@ -1,8 +1,17 @@
+/*
+You are given a string s, you have to find the count of all such substrings which start and ends with the same character.
+
+Input: abcab
+Output: 7
+
+Substrings of abcab are - a,abca,b,bcab,c,a,b
+ */
+
 public class Main {
     public static void main(String[] args) {
 
     }
     public static int substring(String s){
-        
+
     }
 }
