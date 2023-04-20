2760133aae7de7524ccce810dee05bfd7271ca8f
Parth Bisht <bishtparth795@gmail.com>
2023-04-20 16:32:55 +0530

    Check Palindrome String

--- Check Palindrome/Main.java
+++ Check Palindrome/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        
+        String s = "Radar";
+        System.out.println(checkPalindrome(s));
+    }
+    public static boolean checkPalindrome(String s){
+        String reverseString = "";
+        for(int i=s.length()-1;i>=0;i--){
+            reverseString += s.charAt(i);
+        }
+        if(reverseString.toLowerCase().equals(s.toLowerCase())) return true;
+        return false;
     }
 }
