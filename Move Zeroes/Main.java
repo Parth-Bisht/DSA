e1c550804107b2fdc47cdf4f1a96984f30814354
Parth Bisht <bishtparth795@gmail.com>
2023-04-24 19:51:42 +0530

    move zeroes

--- Move Zeroes/Main.java
+++ Move Zeroes/Main.java
@@ -7,11 +7,26 @@ Input: nums = [0,1,0,3,12]
 Output: [1,3,12,0,0]
  */
 
+import java.util.Arrays;
+
 public class Main {
+    public static int[] arr;
     public static void main(String[] args) {
-
+        arr = new int[]{0, 1, 2, 3, 0,4};
+        moveZeroes();
+        System.out.println(Arrays.toString(arr));
     }
-    public static void moveZeroes(int[] arr){
-
+    public static void moveZeroes(){
+        int i = 0, j=0;
+        while(j<arr.length){
+            if(arr[j]==0) j++;
+            else{
+                int temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+                i++;
+                j++;
+            }
+        }
     }
 }
