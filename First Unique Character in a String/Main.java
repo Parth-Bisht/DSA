e631d8f390b4257cb858331a54e53ed8cf570ba1
Parth Bisht <bishtparth795@gmail.com>
2023-04-26 11:36:59 +0530

    solution

--- First Unique Character in a String/Main.java
+++ First Unique Character in a String/Main.java
@@ -13,7 +13,8 @@ Output: -1
  */
 public class Main {
     public static void main(String[] args) {
-
+        String s = "leetcode";
+        System.out.println(unique(s));
     }
     public static int unique(String s){
         int len = s.length();
@@ -23,7 +24,7 @@ public class Main {
             arr[c-'a']++;
         }
         for(int i=0;i<len;i++){
-            if(arr[s.charAt(i)]==1) return i;
+            if(arr[s.charAt(i)-'a']==1) return i;
         }
         return -1;
     }
