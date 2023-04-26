3dbb45f28f2ff810966a4e720393f1f54a159f4f
Parth Bisht <bishtparth795@gmail.com>
2023-04-26 10:59:19 +0530

    hamming distance

--- Hamming Distance/Main.java
+++ Hamming Distance/Main.java
@@ -1,5 +1,24 @@
+/*
+The Hamming distance between two integers is the number of positions at which the corresponding bits are different.
+
+Given two integers x and y, return the Hamming distance between them.
+
+Input: x = 1, y = 4
+Output: 2
+Explanation:
+1   (0 0 0 1)
+4   (0 1 0 0)
+       ↑   ↑
+The above arrows point to positions where the corresponding bits are different.
+
+Input: x = 3, y = 1
+Output: 1
+ */
 public class Main {
     public static void main(String[] args) {
-        
+
+    }
+    public static int hammingDistance(int x,int y){
+
     }
 }
