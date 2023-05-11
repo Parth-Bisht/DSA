fc48db6f6616b0ee6b7326f0824c97e27dfdd45e
Parth Bisht <bishtparth795@gmail.com>
2023-05-11 22:43:08 +0530

    sum of unique digits

--- Sum of Unique Elements/Main.java
+++ Sum of Unique Elements/Main.java
@@ -1,9 +1,21 @@
-//You are given an integer array nums.
-// The unique elements of an array are the elements that appear exactly once in the array.
-//  Return the sum of all the unique elements of nums.
+/* You are given an integer array nums.
+The unique elements of an array are the elements that appear exactly once in the array.
+ Return the sum of all the unique elements of nums.
+
+ Input: nums = [1,2,3,2]
+Output: 4
+Explanation: The unique elements are [1,3], and the sum is 4.
+*/
+
+
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
+        int[] nums = {1,2,3,2};
+        System.out.println(Arrays.toString(nums));
+    }
+    public static int uniqueNumber(int nums[]){
 
     }
 }
