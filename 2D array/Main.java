6c8760d3eb7a070b863361ba58a743d2ac2681c2
Parth Bisht <bishtparth795@gmail.com>
2023-05-12 12:41:02 +0530

    solution iterate over 2D array

--- 2D array/Main.java
+++ 2D array/Main.java
@@ -10,8 +10,26 @@ Sample Output:
 4 3 2 1 5 6 7 8 12 11 10 9
  */
 
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
+        int[][] nums = {{1,2,3,4},{8,7,6,5},{9,10,11,12}};
+        int N = nums.length;
+        int M = nums[0].length;
+        System.out.println(traverse2dArray(nums,N,M));
+    }
 
+    public static List<Integer> traverse2dArray(int[][] nums, int N, int M){
+        List<Integer> ans = new ArrayList<Integer>();
+        for(int i=0;i<M;i++){
+            for(int j=N-1;j>=0;j--){
+                ans.add(nums[j][i]);
+            }
+        }
+        return ans;
     }
 }
