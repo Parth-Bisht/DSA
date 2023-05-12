db725a8ee6eaa95c0008748cb30a2f6111293d30
Parth Bisht <bishtparth795@gmail.com>
2023-05-12 12:28:50 +0530

    down to up in 2D array

--- 2D array/Main.java
+++ 2D array/Main.java
@@ -1,5 +1,17 @@
+/*
+Sample Input:
+4 3
+1 8 9
+2 7 10
+3 6 11
+4 5 12
+
+Sample Output:
+4 3 2 1 5 6 7 8 12 11 10 9
+ */
+
 public class Main {
     public static void main(String[] args) {
-        
+
     }
 }
