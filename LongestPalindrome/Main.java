0b6f2fa733e587afed37a321623dc655afdc1e55
Parth Bisht <bishtparth795@gmail.com>
2023-05-29 15:14:05 +0530

    Longest Palindromic substring

---
+++ LongestPalindrome/Main.java
@@ -0,0 +1,12 @@
+/*
+You are provided a string S.
+Write a program that return the length of the longest palindromic substring.
+
+Sample input: thisracecarisgood
+Output: 7
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
