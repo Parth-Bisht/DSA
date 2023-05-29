e3f2b5e532013bc4d4e84efd9adab54844c82c49
Parth Bisht <bishtparth795@gmail.com>
2023-05-29 15:21:39 +0530

    check palindrome function

--- LongestPalindrome/Main.java
+++ LongestPalindrome/Main.java
@@ -9,4 +9,16 @@ public class Main {
     public static void main(String[] args) {
 
     }
+    public static int checkLongestPalindrome(String s){
+        
+    }
+
+    public static boolean checkPalindrome(String str, int length){
+        String reverse = "";
+        for(int i=length-1;i>=0;i--){
+            reverse += str.charAt(i);
+        }
+        if(reverse.equals(str)) return true;
+        return false;
+    }
 }
