02092a2acce67ccdc67f6efa5ed824f9f059d2eb
Parth Bisht <bishtparth795@gmail.com>
2023-05-30 12:35:25 +0530

    Distibute Candies

---
+++ Distribute Candies/Main.java
@@ -0,0 +1,22 @@
+/*
+There are n students standing in a line. Each student is assigned a rating value.
+
+You are responsible to distribute the candies to these students subjected to the following requirements:
+
+-> Each student must have at least one candy because every student deserves it.
+
+-> Students with a strictly higher rating get more candies than their neighbors.
+
+What is the minimum number of candies you need to distribute to the student?
+
+input:
+3
+1 0 2
+output: 5
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+    
+}
