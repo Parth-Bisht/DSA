899c2e85e08a576345e66f89a325bed720b07861
Parth Bisht <bishtparth795@gmail.com>
2023-05-30 11:42:46 +0530

    String value

---
+++ String Value/Main.java
@@ -0,0 +1,9 @@
+/*
+In this problem, we have to find the sum of all the values associated with individual characters
+in the string, such that the value associated with ais 0,bis 1,cis 2, and so on.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
