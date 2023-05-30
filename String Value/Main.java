efd1af159f604bba75e95a18b4d4ab9a45c97c34
Parth Bisht <bishtparth795@gmail.com>
2023-05-30 11:46:36 +0530

    string value answer

--- String Value/Main.java
+++ String Value/Main.java
@@ -4,13 +4,14 @@ in the string, such that the value associated with ais 0,bis 1,cis 2, and so on.
  */
 public class Main {
     public static void main(String[] args) {
-
+        String s = "aba";
+        System.out.println(stringValue(s));
     }
     public static int stringValue(String s){
         int sum = 0;
         for(int i=0;i<s.length();i++){
             int temp = (int) s.charAt(i);
-            sum += temp;
+            sum += temp-96;
         }
         return sum;
     }
