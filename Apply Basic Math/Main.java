6e419f9ceed151a1dc3e2bce7b25c51d83b30fed
Parth Bisht <bishtparth795@gmail.com>
2023-05-31 13:01:09 +0530

    Basic Math

---
+++ Apply Basic Math/Main.java
@@ -0,0 +1,22 @@
+/*
+You are given an array A with N elements. You are allowed to remove only one element, which makes the
+sum of all the remaining elements exactly divisible by 7
+
+Your task is to find the first index of smallest element that can be removed from array.
+If there is no answer print -1
+
+Sample Input:
+5
+14 7 8 2 4
+
+Output: 1
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public static int math(int N, int[] arr){
+
+    }
+}
