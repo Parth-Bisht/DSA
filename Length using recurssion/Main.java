190c232f2a8ec2a3daa367af82facd990c656357
Parth Bisht <bishtparth795@gmail.com>
2023-06-01 10:30:36 +0530

    length using recurrsion

---
+++ Length using recurssion/Main.java
@@ -0,0 +1,13 @@
+/*
+Given a string, S. Find the length of the string using recursion.
+Note: You are not allowed to use the length built-in property.
+
+Sample input: parthbisht
+output: 10
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+    
+}
