2ff33e1188c0301c5f1d69a2fa16479776731fba
Parth Bisht <bishtparth795@gmail.com>
2023-06-01 10:36:27 +0530

    recurssion length

--- Length using recurssion/Main.java
+++ Length using recurssion/Main.java
@@ -7,11 +7,12 @@ output: 10
  */
 public class Main {
     public static void main(String[] args) {
-
+        String str = "parthbisht";
+        System.out.println(findLength(str,0));
     }
 
     public static int findLength(String str, int i){
-        if(str.charAt(i)=='\0') return 0;
+        if(i>str.length()) return 0;
         return 1+findLength(str,i+1);
     }
 }
