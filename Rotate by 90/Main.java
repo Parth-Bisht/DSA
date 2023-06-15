05aabb3060de9b1a8cbfd6549b058c398e8e4f35
Parth Bisht <bishtparth795@gmail.com>
2023-06-15 12:39:09 +0530

    rotate by 90

---
+++ Rotate by 90/Main.java
@@ -0,0 +1,22 @@
+/*
+Given a square matrix of size N , turn it by 90 degrees in an anti-clockwise direction,
+as shown in sample input.
+
+Sample input:
+4
+1 2 3 4
+5 6 7 8
+1 2 3 4
+5 6 7 8
+
+Sample output:
+4 8 4 8
+3 7 3 7
+2 6 2 6
+1 5 1 5
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
