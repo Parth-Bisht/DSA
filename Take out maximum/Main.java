f8126066b520d1c8b90ab9c4b06011db8b2bc6bc
Parth Bisht <bishtparth795@gmail.com>
2023-06-16 12:27:47 +0530

    maximum

---
+++ Take out maximum/Main.java
@@ -0,0 +1,13 @@
+/*
+Given an array of integers and a number k, find the maximum sum of a subarray of size k.
+Sample Input:
+10 3
+-1 -1 -2 1 -2 4 1 9 3 9
+Sample output:
+21
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
