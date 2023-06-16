14352ad0e3443c12920bf76606293b5bae944cb7
Parth Bisht <bishtparth795@gmail.com>
2023-06-16 12:36:29 +0530

    find out maximum solution

--- Take out maximum/Main.java
+++ Take out maximum/Main.java
@@ -10,9 +10,18 @@ public class Main {
     public static void main(String[] args) {
         int n = 10, k = 3;
         int[] arr = new int[]{-1,-1,-2,1,-2,4,1,9,3,9};
-
+        System.out.println(slidingWindow(n,k,arr));
     }
     public static int slidingWindow(int n,int k,int[] arr){
-        
+        int sum = 0;
+        for(int i=0;i<k;i++){
+            sum += arr[i];
+        }
+        int max = sum;
+        for(int i=k;i<n;i++){
+            sum = sum + arr[i] - arr[i-k];
+            max = Math.max(sum,max);
+        }
+        return max;
     }
 }
