4aff71f1aba6d646e082d05c4183794f21358b58
Parth Bisht <bishtparth795@gmail.com>
2023-06-16 12:16:51 +0530

    two sum

--- Two Sum/Main.java
+++ Two Sum/Main.java
@@ -7,16 +7,16 @@ Input: nums = [3,2,4], target = 6
 Output: [1,2]
  */
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        int nums[] = {3,2,4};
-        int target = 6;
+        int nums[] = {1,2,3,5,5};
+        int target = 10;
 
-        int[] ans = twoSum(nums,target);
+//        int[] ans = twoSum(nums,target);
+//        System.out.println(Arrays.toString(ans));
+        int[] ans = twoPointer(nums,target);
         System.out.println(Arrays.toString(ans));
     }
     public static int[] twoSum(int nums[], int target){
@@ -28,5 +28,17 @@ public class Main {
             }
         }
         return new int[] {};
+    }
+
+    public static int[] twoPointer(int nums[], int target){
+        int i=0, j=nums.length-1;
+        while(i<j){
+            if(nums[i]+nums[j]==target) {
+                return new int[]{i,j};
+            }
+            else if(nums[i]+nums[j]>target) j--;
+            else i++;
+        }
+        return new int[]{-1,-1};
     }
 }
