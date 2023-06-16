715f981c5953ebc416dfea9110ab627089337175
Parth Bisht <bishtparth795@gmail.com>
2023-06-16 12:33:02 +0530

    sliding winndow

--- Take out maximum/Main.java
+++ Take out maximum/Main.java
@@ -8,6 +8,11 @@ Sample output:
  */
 public class Main {
     public static void main(String[] args) {
+        int n = 10, k = 3;
+        int[] arr = new int[]{-1,-1,-2,1,-2,4,1,9,3,9};
+
+    }
+    public static int slidingWindow(int n,int k,int[] arr){
         
     }
 }
