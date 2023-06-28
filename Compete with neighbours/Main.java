7118efa1a5925b2906ebe53a1857f74738215835
Parth Bisht <bishtparth795@gmail.com>
2023-06-28 17:45:54 +0530

    compete with neighbour

---
+++ Compete with neighbours/Main.java
@@ -0,0 +1,6 @@
+/*
+ You are provided an array A which has N elements.
+ Your task is to find the count of such occurrence where the element is larger than its neighbour.
+ */
+public class Main {
+}
