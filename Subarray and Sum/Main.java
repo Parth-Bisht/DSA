1f27e4cc240cade52f4b5ddccddd57678ade6bde
Parth Bisht <bishtparth795@gmail.com>
2023-06-28 18:18:05 +0530

    subarray

--- Subarray and Sum/Main.java
+++ Subarray and Sum/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /*
 Given an array of integers of length n and a positive integer K,
 the task is to find the count of the longest possible subarrays with the sum of its elements not divisible by K.
@@ -11,6 +13,22 @@ Sample Output:
  */
 public class Main {
     public static void main(String[] args) {
-        
+        int n = 4;
+        int k = 3;
+        int[] arr = {2,3,4,6};
+    }
+
+    public static int subarrayAndSum(int n,int k,int[] arr){
+        int s = Arrays.stream(arr).sum();
+        if(s%k==0) return 1;
+        else{
+            for(int i=0;i<n;i++){
+                int[] temp = {arr[i]};
+                int sum = arr[i];
+                for(int j=i+1;j<n;j++){
+                     
+                }
+            }
+        }
     }
 }
