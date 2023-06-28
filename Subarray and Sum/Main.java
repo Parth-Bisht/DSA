d1a73b431dfa5d36aa244a1657a60dd194b9fe2c
Parth Bisht <bishtparth795@gmail.com>
2023-06-28 17:53:45 +0530

    Subarray and sum

---
+++ Subarray and Sum/Main.java
@@ -0,0 +1,16 @@
+/*
+Given an array of integers of length n and a positive integer K,
+the task is to find the count of the longest possible subarrays with the sum of its elements not divisible by K.
+
+Sample Input:
+4 3
+2 3 4 6
+
+Sample Output:
+1
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
