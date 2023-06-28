f044571ab2432cd31cf1ce6835befac951488c6e
Parth Bisht <bishtparth795@gmail.com>
2023-06-28 17:48:53 +0530

    solution

--- Compete with neighbours/Main.java
+++ Compete with neighbours/Main.java
@@ -1,6 +1,20 @@
 /*
  You are provided an array A which has N elements.
  Your task is to find the count of such occurrence where the element is larger than its neighbour.
+
+ Sample Input:
+ 8
+ 1 2 3 4 2 1 6 5
+
+ Sample Output:
+ 2
  */
 public class Main {
+    public static void main(String[] args) {
+        int n = 8;
+        int[] arr = {1,2,3,4,2,1,6,5};
+    }
+    public static int compete(){
+        
+    }
 }
