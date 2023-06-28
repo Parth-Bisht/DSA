d8495b9f1c0fc1e93578fbdc5740d1002804a35a
Parth Bisht <bishtparth795@gmail.com>
2023-06-28 18:34:51 +0530

    subarray and sum

--- Subarray and Sum/Main.java
+++ Subarray and Sum/Main.java
@@ -19,16 +19,33 @@ public class Main {
     }
 
     public static int subarrayAndSum(int n,int k,int[] arr){
+        int max = 0;
+        int count = 0;
         int s = Arrays.stream(arr).sum();
-        if(s%k==0) return 1;
+        if(s%k==1) return 1;
         else{
             for(int i=0;i<n;i++){
-                int[] temp = {arr[i]};
+                int[] temp = new int[n];
+                temp[0] = arr[i];
                 int sum = arr[i];
                 for(int j=i+1;j<n;j++){
-                     
+                     temp[j] = arr[j];
+                     sum += arr[j];
+                     if(sum%k==1){
+                         max = Math.max(max,temp.length);
+                     }
                 }
             }
+           int temp_sum = 0;
+            for(int i=0;i<max;i++){
+                temp_sum += arr[i];
+            }
+            if(temp_sum%k==1) count++;
+            for(int i=max;i<n;i++){
+                temp_sum = temp_sum+arr[i]-arr[i-max];
+                if(temp_sum%k==1) count++;
+            }
         }
+        return count;
     }
 }
