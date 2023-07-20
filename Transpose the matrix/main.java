0b3d955f9be3850f71e14447dd5f844f7a21c4fc
Parth Bisht <bishtparth795@gmail.com>
2023-07-20 12:24:20 +0530

    transpose of a matrix

---
+++ Transpose the matrix/main.java
@@ -0,0 +1,29 @@
+/*
+The transpose of a matrix is an operator which flips a matrix over its diagonal,
+that is it switches the row and column of the matrix by producing another matrix.
+
+See sample test case for better understanding
+
+So, given an array of n rows and m columns, generate the transpose of the matrix.
+
+sample input:
+5 4
+0 0 0 0
+1 1 1 1
+2 2 2 2
+3 3 3 3
+4 4 4 4
+
+sample output:
+0 1 2 3 4
+0 1 2 3 4
+0 1 2 3 4
+0 1 2 3 4
+
+ */
+public class main {
+    public static void main(String[] args) {
+        int n = 5,m=4;
+        int[][] matrix = {{0,0,0,0},{1,1,1,1},{2,2,2,2},{3,3,3,3},{4,4,4,4}};
+    }
+}
