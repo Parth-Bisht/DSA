768e625b1625e2169c60978faf7c779d8d030ace
Parth Bisht <bishtparth795@gmail.com>
2023-07-21 15:33:40 +0530

    Find duplicates number solution

--- Find Duplicate/main.java
+++ Find Duplicate/main.java
@@ -15,21 +15,23 @@ Output: 3
  */
 public class main {
     public static void main(String[] args) {
-
+        int[] nums = new int[]{3,1,3,4,2};
+        int output = duplicateNumber(nums);
+        System.out.println(output);
     }
     public static int duplicateNumber(int[] nums){
-        int slow = nums[0];
-        int fast = nums[0];
-        do{
-            slow = nums[slow];
-            fast = nums[nums[fast]];
-        }while(slow!=fast);
+        int tortoise = nums[0];
+        int hare = nums[nums[0]];
 
-        slow = 0;
-        while(slow!=fast){
-            slow = nums[slow];
-            fast = nums[fast];
+        while(tortoise!=hare){
+            tortoise = nums[tortoise];
+            hare = nums[nums[hare]];
         }
-        return slow;
+        tortoise = 0;
+        while(tortoise!=hare){
+            tortoise = nums[tortoise];
+            hare = nums[hare];
+        }
+        return tortoise;
     }
 }
