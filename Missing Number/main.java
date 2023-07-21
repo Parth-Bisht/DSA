0ac9e156edaeb432cd9e5470a51be3bcdbc22891
Parth Bisht <bishtparth795@gmail.com>
2023-07-21 13:36:14 +0530

    Missing Numbers

--- Missing Number/main.java
+++ Missing Number/main.java
@@ -1,5 +1,18 @@
+/*
+Given an array nums containing n distinct numbers in the range [0, n],
+return the only number in the range that is missing from the array.
+
+Example 1:
+Input: nums = [3,0,1]
+Output: 2
+Explanation: n = 3 since there are 3 numbers, so all numbers are in the range [0,3].
+2 is the missing number in the range since it does not appear in nums.
+ */
 public class main {
     public static void main(String[] args) {
+
+    }
+    public static int missigNumber(int[] nums){
         
     }
 }
