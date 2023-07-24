fcb95bcdc15e30fa90f7b54c21dbbbcf5bd61cda
Parth Bisht <bishtparth795@gmail.com>
2023-07-24 17:44:54 +0530

    changes

--- Repeated Substring Pattern/main.java
+++ Repeated Substring Pattern/main.java
@@ -9,17 +9,20 @@ Explanation: It is the substring "ab" twice.
  */
 public class main {
     public static void main(String[] args) {
-
+        String s = "aba";
+        System.out.println(test(s));
     }
     public static boolean test(String s){
         int n = s.length();
-        for(int i=1;i<=n;i++){
-            String curr = s.substring(0,1);
-            int j=i;
-            while(j<n && s.substring(j,j+1).equals(curr)){
-                j += i;
+        for(int i=1;i<=n/2;i++){
+            if(n%2==0) {
+                String curr = s.substring(0, 1);
+                int j = i;
+                while (j < n && s.substring(j, j + 1).equals(curr)) {
+                    j += i;
+                }
+                if(j==n) return true;
             }
-            if(j==n) return true;
         }
         return false;
     }
