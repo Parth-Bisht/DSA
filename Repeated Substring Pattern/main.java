a58f012bbf1f97be2c0f3c05632346a5ff2b3d75
Parth Bisht <bishtparth795@gmail.com>
2023-07-24 17:35:04 +0530

    Repeated substring

---
+++ Repeated Substring Pattern/main.java
@@ -0,0 +1,11 @@
+/*
+Given a string s, check if it can be constructed by taking
+a substring of it and appending multiple copies of the substring together.
+
+Example:
+Input: s = "abab"
+Output: true
+Explanation: It is the substring "ab" twice.
+ */
+public class main {
+}
