cabaf52c6cff8ecdadb753d088de142a45f25ec9
Parth Bisht <bishtparth795@gmail.com>
2023-07-27 15:08:20 +0530

    solution

--- Reverse Integer/main.java
+++ Reverse Integer/main.java
@@ -12,9 +12,17 @@ Output: -321
  */
 public class main {
     public static void main(String[] args) {
-
+        int x= -123;
+        System.out.println(reverseTheNumber(x));
     }
-    public static int reverse(int x){
-        
+    public static int reverseTheNumber(int x){
+        long reverse = 0;
+        while(x!=0){
+            int rem = x%10;
+            reverse = reverse*10+rem;
+            x = x/10;
+        }
+        if(reverse>Integer.MAX_VALUE || reverse<Integer.MIN_VALUE) return 0;
+        return (int) reverse;
     }
 }
