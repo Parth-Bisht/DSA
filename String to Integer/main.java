c39e503d64f2acc1c5bec52c5453c05769bbf070
Parth Bisht <bishtparth795@gmail.com>
2023-07-27 16:47:18 +0530

    solution

--- String to Integer/main.java
+++ String to Integer/main.java
@@ -35,5 +35,21 @@ public class main {
     public static int myAtoi(String s){
         int len = s.length();
         if(len == 0) return 0;
+        int i=0;
+        double num = 0;
+        while(i<len && s.charAt(i)==' ')i++;
+        if(i==len) return 0;
+        boolean isPositive = s.charAt(i)=='+';
+        boolean isNegative = s.charAt(i)=='-';
+        if(isPositive) i++;
+        if(isNegative) i++;
+        while(i<len && s.charAt(i)>='0' && s.charAt(i)<='9'){
+            num = num*10 + (s.charAt(i)-'0');
+            i++;
+        }
+        num = isNegative?-num:num;
+        num = num>Integer.MAX_VALUE?Integer.MAX_VALUE:num;
+        num = num<Integer.MIN_VALUE?Integer.MIN_VALUE:num;
+        return (int) num;
     }
 }
