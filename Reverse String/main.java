c24b4b614deb2240bd328396d0da080b0c015269
Parth Bisht <bishtparth795@gmail.com>
2023-07-31 12:33:05 +0530

    solution

--- Reverse String/main.java
+++ Reverse String/main.java
@@ -15,5 +15,14 @@ public class main {
     public static void main(String[] args) {
 
     }
-
+    public static void reverseString(char[] s){
+        int i=0,j=s.length-1;
+        while(i<j){
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+            i++;
+            j--;
+        }
+    }
 }
