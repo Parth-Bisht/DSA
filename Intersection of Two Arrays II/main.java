be1bd5aa7fa8c7a58dd98e0ddaed4430ab877084
Parth Bisht <bishtparth795@gmail.com>
2023-07-31 13:03:55 +0530

    INtersection of arrays II

--- Intersection of Two Arrays II/main.java
+++ Intersection of Two Arrays II/main.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /*
 Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must appear
 as many times as it shows in both arrays and you may return the result in any order.
@@ -13,6 +16,30 @@ Explanation: [9,4] is also accepted.
  */
 public class main {
     public static void main(String[] args) {
-        
+
+    }
+    public static int[] intersect(int[] nums1,int[] nums2){
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        ArrayList<Integer> temp = new ArrayList<>();
+        int i=0,j=0;
+        while(i<nums1.length && j<nums2.length){
+            if(nums1[i]<nums2[j]){
+                i++;
+            } else if (nums2[j]<nums1[i]) {
+                j++;
+            }else{
+                temp.add(nums1[i]);
+                i++;
+                j++;
+            }
+        }
+        int[] output = new int[temp.size()];
+        int k=0;
+        while(k<temp.size()){
+            output[k] = temp.get(k);
+            k++;
+        }
+        return output;
     }
 }
