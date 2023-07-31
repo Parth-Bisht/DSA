ec7545168a2bea55569785adffdee949f5c67563
Parth Bisht <bishtparth795@gmail.com>
2023-07-31 12:31:55 +0530

    reverse string

---
+++ Reverse String/main.java
@@ -0,0 +1,19 @@
+/*
+Write a function that reverses a string. The input string is given as an array of characters s.
+
+You must do this by modifying the input array in-place with O(1) extra memory.
+
+Example:
+Input: s = ["h","e","l","l","o"]
+Output: ["o","l","l","e","h"]
+
+Example:
+Input: s = ["H","a","n","n","a","h"]
+Output: ["h","a","n","n","a","H"]
+ */
+public class main {
+    public static void main(String[] args) {
+
+    }
+
+}
