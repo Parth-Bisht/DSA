f4cdd1a7e165e9fc1d4cbabe4d4fb4dcc8771016
Parth Bisht <bishtparth795@gmail.com>
2023-08-04 16:27:34 +0530

    pow(x,n)

--- pow(x,n)/main.java
+++ pow(x,n)/main.java
@@ -1,5 +1,16 @@
+/*
+Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
+
+Example:
+Input: x = 2.00000, n = 10
+Output: 1024.00000
+
+Example:
+Input: x = 2.10000, n = 3
+Output: 9.26100
+ */
 public class main {
     public static void main(String[] args) {
-        
+
     }
 }
