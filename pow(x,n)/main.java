9fd6a0931421b613bc51960db87502bce5d9ea77
Parth Bisht <bishtparth795@gmail.com>
2023-08-04 16:32:45 +0530

    pow solution

--- pow(x,n)/main.java
+++ pow(x,n)/main.java
@@ -11,12 +11,14 @@ Output: 9.26100
  */
 public class main {
     public static void main(String[] args) {
-
+        double x = 2.00000;
+        int n = -2;
+        System.out.println(pow(x,(long) n));
     }
     private static double pow(double x, long n){
         if(n==0) return 1;
         if(n<0) return 1.0/pow(x,-n);
-        if(n%2==1) return pow(x*x,(n-1)/2);
+        if(n%2==1) return x*pow(x*x,(n-1)/2);
         else return pow(x*x,n/2);
     }
 }
