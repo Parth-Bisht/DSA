e3bf10975cb4dd5434d93e7fb8dba083b27dfea8
Parth Bisht <bishtparth795@gmail.com>
2023-08-14 22:27:05 +0530

    3Sum

--- 3Sum/Main.java
+++ 3Sum/Main.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 /*
 Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]]
 such that i != j, i != k, and j != k,and nums[i] + nums[j] + nums[k] == 0.
@@ -13,9 +15,20 @@ nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
 The distinct triplets are [-1,0,1] and [-1,-1,2].
 Notice that the order of the output and the order of the triplets does not matter.
 
+Input: nums = [0,1,1]
+Output: []
+Explanation: The only possible triplet does not sum up to 0.
+
+Input: nums = [0,0,0]
+Output: [[0,0,0]]
+Explanation: The only possible triplet sums up to 0.
+
  */
 public class Main {
     public static void main(String[] args) {
+
+    }
+    public static List<List<Integer>> threeSum(int[] nums){
 
     }
 }
