4b66d30fb88723f945b9ea06aeb7bc7741e7b9a4
Parth Bisht <bishtparth795@gmail.com>
2023-08-15 22:23:08 +0530

    3Sum

--- 3Sum/Main.java
+++ 3Sum/Main.java
@@ -1,3 +1,4 @@
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -28,11 +29,32 @@ Explanation: The only possible triplet sums up to 0.
  */
 public class Main {
     public static void main(String[] args) {
-
+        int[] nums = new int[] {-1,0,1,2,-1,-4};
+        threeSum(nums);
     }
     public static List<List<Integer>> threeSum(int[] nums){
         List<List<Integer>> ans = new ArrayList<>();
         Arrays.sort((nums));
+        for(int i=0;i<nums.length-2;i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            int j = i+1;
+            int k = nums.length - 1;
+
+            while(j<k){
+                int sum = nums[i]+nums[j]+nums[k];
+                if(sum==0){
+                    ans.add(Arrays.asList(nums[i],nums[j],nums[k]));
+                    while(j<k && nums[j]!=nums[j+1]) j++;
+                    while(k>j && nums[k]!=nums[k-1]) k--;
+                    j++;
+                    k--;
+                } else if (sum<0) {
+                    j++;
+                }else{
+                    k--;
+                }
+            }
+        }
         return ans;
     }
 }
