10f5e45ccf6934ea1e357188046c7d2923deb881
Parth Bisht <bishtparth795@gmail.com>
2023-09-06 12:53:10 +0530

    Combination sum II solution

--- Combination Sum II/src/Main.java
+++ Combination Sum II/src/Main.java
@@ -36,10 +36,18 @@ public class Main {
     public static List<List<Integer>> combinationSum(int[] nums,int target){
         List<List<Integer>> list = new ArrayList<>();
         Arrays.sort(nums);
+        backtrack(list,new ArrayList<>(),nums,target,0);
         return list;
     }
 
     private static void backtrack(List<List<Integer>> list,List<Integer> temp,int[] nums,int target,int start){
-        
+        if(target<0) return;
+        else if (target==0) list.add(new ArrayList<>(temp));
+        for(int i=start;i<nums.length;i++){
+            if(i>start && nums[i]==nums[i-1]) continue;
+            temp.add(nums[i]);
+            backtrack(list,temp,nums,target-nums[i],i+1);
+            temp.remove(temp.size()-1);
+        }
     }
 }
