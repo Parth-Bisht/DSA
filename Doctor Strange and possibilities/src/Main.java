f4faee0902209c4ef02eccb3cfc0a100710109fa
Parth Bisht <bishtparth795@gmail.com>
2023-09-11 15:50:18 +0530

    doctor strange and possibilities

---
+++ Doctor Strange and possibilities/src/Main.java
@@ -0,0 +1,22 @@
+/*
+Dr. Strange has n distinct integers from 1 to n. He uses his mind power and time stone to calculate all the possible
+subsets using the given n integers. Generate all the possible subsets.
+Elements within the set must be in non-decreasing order. All the subsets must be distinct and sorted in ascending
+order or lexicographically. For an empty subset just print a blank line.
+
+Sample Input: 3
+
+Sample Output:
+
+1
+1 2
+1 2 3
+1 3
+2
+2 3
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
