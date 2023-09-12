2103962c52e6a0ae77c3a01b73e3d87a465047a8
Parth Bisht <bishtparth795@gmail.com>
2023-09-12 23:19:33 +0530

    Doctor strange and possibilities II

--- Doctor strange and Possibilities II/src/Main.java
+++ Doctor strange and Possibilities II/src/Main.java
@@ -1,3 +1,20 @@
+/*
+Dr. Strange is back again and this time he has an array A with n integers which may have duplicates.
+So he again uses his mind power and time stone to calculate all the possible subsets of the input array A.
+Generate all the possible subsets.
+
+sample input:
+3
+1 2 2
+
+sample output:
+
+1
+1 2
+1 2 2
+2
+2 2
+ */
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
