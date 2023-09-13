023e2b7d0f0be56450d592953b0e8eb76f0a3057
Parth Bisht <bishtparth795@gmail.com>
2023-09-13 21:39:52 +0530

    doctor strange solution

--- Doctor strange and Possibilities II/src/Main.java
+++ Doctor strange and Possibilities II/src/Main.java
@@ -20,13 +20,21 @@ sample output:
 public class Main {
     public static void main(String[] args) {
         int n = 3;
-        ArrayList<Integer> arr = new ArrayList<>();
+        ArrayList<Integer> arr = new ArrayList<>(3);
         arr.add(1);
         arr.add(2);
         arr.add(2);
     }
 
     public static void strangeII(ArrayList<Integer> arr,ArrayList<Integer> tempArr, int index){
-    
+        if(tempArr.size()>=0){
+            System.out.println(tempArr);
+        }
+        if(index==arr.size()) return;
+        for(int i=index;i<arr.size();i++){
+            tempArr.add(arr.get(i));
+            strangeII(arr,tempArr,i+1);
+            tempArr.remove(tempArr.size()-1);
+        }
     }
 }
