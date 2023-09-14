394a4d8ea904dbff4a1d851ea17e88afc9eac42d
Parth Bisht <bishtparth795@gmail.com>
2023-09-14 22:17:37 +0530

    n traversal

--- N traversal/src/Main.java
+++ N traversal/src/Main.java
@@ -4,6 +4,15 @@ joining the visited indexes resembles anN. If we observe carefully, we have to v
 in reverse fashion, then the elements along the diagonal such that the index of row is equal to the index of column, and
 then finally, we have to traverse the last column, from bottom to top. Care should be taken about the top left corner element,
 and the bottom right corner element, that they are not visited twice.
+
+Sample Input:
+3
+1 2 3
+4 5 6
+7 8 9
+
+Sample Output:
+7 4 1 5 9 6 3
  */
 public class Main {
     public static void main(String[] args) {
