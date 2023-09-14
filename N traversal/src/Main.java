a32d43c12f9894f574023be8991f6f24334053b0
Parth Bisht <bishtparth795@gmail.com>
2023-09-14 22:15:19 +0530

    n traversal

--- N traversal/src/Main.java
+++ N traversal/src/Main.java
@@ -1,3 +1,10 @@
+/*
+In this problem, we are given a square matrix, and we have to traverse the matrix such that the pattern formed after
+joining the visited indexes resembles anN. If we observe carefully, we have to visit all the elements in the first column,
+in reverse fashion, then the elements along the diagonal such that the index of row is equal to the index of column, and
+then finally, we have to traverse the last column, from bottom to top. Care should be taken about the top left corner element,
+and the bottom right corner element, that they are not visited twice.
+ */
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
