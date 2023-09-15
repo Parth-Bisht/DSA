6661c6a3dd838d10dc381469be3ac75e8f7a184d
Parth Bisht <bishtparth795@gmail.com>
2023-09-15 21:51:09 +0530

    n-traversal

--- N traversal/src/Main.java
+++ N traversal/src/Main.java
@@ -16,7 +16,8 @@ Sample Output:
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[][] matrix = new int[][]{{1,2,3},{4,5,6},{7,8,9}};
+        
     }
     public static void nTraversal(int[][] matrix){
 
