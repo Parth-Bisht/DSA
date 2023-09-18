1e7ddc5b913550dc256b2e55e51d45a2004d8517
Parth Bisht <bishtparth795@gmail.com>
2023-09-18 18:44:41 +0530

    specific traversal

--- specific Diagonals/src/Main.java
+++ specific Diagonals/src/Main.java
@@ -1,3 +1,17 @@
+/*
+Given a matrix of size row*col which contains distinct integers and you are given an integer and you need to print
+both the diagonals crossing through the given element , you can always assume that the given element always exist in the matrix.
+
+Row = 3, Col = 3
+1 2 3
+4 5 6
+7 8 9
+
+K = 6
+
+Diagonal 1: 2 6 (Left to right diagonal and from top to bottom order)
+Diagonal 2: 6 8 (Right to left diagonal and from top to bottom order)
+ */
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
