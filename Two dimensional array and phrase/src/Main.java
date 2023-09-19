fb7338683bdca24a9fef7b7601417260fce18f5e
Parth Bisht <bishtparth795@gmail.com>
2023-09-19 12:12:37 +0530

    2D array and phrase

---
+++ Two dimensional array and phrase/src/Main.java
@@ -0,0 +1,20 @@
+/*
+You are given an array of n rows and m columns which contains lower case English letters. How many times does the
+phrase "saba" appear horizontally, vertically, and diagonally in the grid?
+
+Sample Input:
+n=5, m=5
+s a f e r
+a m j a d
+b a b o l
+a a r o n
+s o n g s
+
+Sample output:
+2
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
