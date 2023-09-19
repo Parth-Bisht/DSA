2c0d8c688f2b4cbeef28127b3a0f9c3c0b612dd4
Parth Bisht <bishtparth795@gmail.com>
2023-09-19 11:50:53 +0530

    2D array and sum

---
+++ Two dimensional array and sum/src/Main.java
@@ -0,0 +1,10 @@
+/*
+You are given an array of n rows and m columns which contains positive integers and sum s
+You need to find occurrences of the sum of three consecutive numbers (i.e x,y, and z ) whose sum is equal to s appear
+horizontally, vertically and diagonally in the given matrix.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
