3628323fd3173da2b09637132438a99c15e385b4
Parth Bisht <bishtparth795@gmail.com>
2023-09-19 13:00:48 +0530

    2D array and phrase

--- Two dimensional array and phrase/src/Main.java
+++ Two dimensional array and phrase/src/Main.java
@@ -15,6 +15,35 @@ Sample output:
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        char[][] matrix = new char[][]{{'s','a','f','e','r'},{'a','m','j','a','d'},{'b','a','b','o','l'},{'a','a','r','o','n'},{'s','o','n','g','s'}};
+        int n=5,m=5;
+    }
+    public static int arrayAndPhrase(char[][] matrix,int n,int m){
+        int count = 0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(j<m-3){
+                    String bag = "";
+                    bag = bag + matrix[i][j]+matrix[i][j+1]+matrix[i][j+2]+matrix[i][j+3];
+                    if(bag.equals("saba")) count++;
+                }
+                if(i<n-3){
+                    String bag = "";
+                    bag = bag + matrix[i][j]+matrix[i+1][j]+matrix[i+2][j]+matrix[i+3][j];
+                    if(bag.equals("saba")) count++;
+                }
+                if(i<n-3 && j<m-3){
+                    String bag = "";
+                    bag = bag + matrix[i][j]+matrix[i+1][j+1]+matrix[i+2][j+2]+matrix[i+3][j+3];
+                    if(bag.equals("saba")) count++;
+                }
+                if(i>2 && j<m-3){
+                    String bag = "";
+                    bag = bag + matrix[i][j]+matrix[i-1][j+1]+matrix[i-2][j+2]+matrix[i-3][j+3];
+                    if(bag.equals("saba")) count++;
+                }
+            }
+        }
+        return count;
     }
 }
