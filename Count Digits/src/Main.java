8cb23046d0f2b479182622da80838770893716b5
Parth Bisht <bishtparth795@gmail.com>
2023-09-21 18:39:45 +0530

    count digits

--- Count Digits/src/Main.java
+++ Count Digits/src/Main.java
@@ -1,3 +1,17 @@
+/*
+You are given an array, stored in a variable with the size arr
+The size of the array is stored in a variable with the name n
+You have to find the count of 0's ,1's and 2's
+For example, consider the array as arr = [0 1 0 2 1 2 0 1 2 0], and the value stored in n = 10
+Then, the required output will be 4,3,3 , as no. of 0's are 4, no. of 1's are 3, and no. of 2's are 3.
+
+Sample input:
+10
+0 1 0 1 1 1 0 2 2 0
+
+output:
+4 4 2
+ */
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
