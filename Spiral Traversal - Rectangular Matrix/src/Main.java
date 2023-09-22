f433511571e4a2e39e539df43572a1c369e55c65
Parth Bisht <bishtparth795@gmail.com>
2023-09-22 17:30:10 +0530

    spiral traversal

--- Spiral Traversal - Rectangular Matrix/src/Main.java
+++ Spiral Traversal - Rectangular Matrix/src/Main.java
@@ -1,3 +1,19 @@
+/*
+In this question, we are given a 2D matrix, such that we have to print the spiral traversal of the matrix.
+First we have to move along the left-most unvisited column, followed by the bottom-most unvisited column,
+followed by the right-most unvisited column, and finally traversing the top-most unvisited row of the matrix.
+The important thing here is to modify the boundaries, every time a row or column is visited. This is to be done till
+the point, all the elements in the matrix are visited.
+
+Sample Input:
+3 4
+1 2 3 4
+5 6 7 8
+9 10 11 12
+
+Sample Output:
+1 5 9 10 11 12 8 4 3 2 6 7
+ */
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
