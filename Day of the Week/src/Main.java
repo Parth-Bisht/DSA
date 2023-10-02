83e6481e48202ef956cb35f1f4b4f0c2c312aa57
Parth Bisht <bishtparth795@gmail.com>
2023-10-02 23:00:00 +0530

    week days

--- Day of the Week/src/Main.java
+++ Day of the Week/src/Main.java
@@ -1,3 +1,14 @@
+/*
+In this question, you are given a particular day, and an integer N.
+You have to find out what day will it be, after N days.
+
+Sample Input:
+Wednesday
+8
+Output:
+Thursday
+
+ */
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
