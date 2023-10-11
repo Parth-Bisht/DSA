b1a823e6c2080dfcddc5f9a6a42229eacca79a9a
Parth Bisht <bishtparth795@gmail.com>
2023-10-11 16:59:21 +0530

    day in week

--- Day of the Week/src/Main.java
+++ Day of the Week/src/Main.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /*
 In this question, you are given a particular day, and an integer N.
 You have to find out what day will it be, after N days.
@@ -11,9 +14,24 @@ Thursday
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<String> week = new ArrayList<>();
+        week.add("Monday");
+        week.add("Tuesday");
+        week.add("Wednesday");
+        week.add("Thursday");
+        week.add("Friday");
+        week.add("Saturday");
+        week.add("Sunday");
     }
-    public static String DayWeek(String day, int n){
+    public static String DayWeek(String day, int n, List<String> week){
+        while(n>7){
+            n=n%7;
+        }
+        for(int i=0;i<week.size();i++){
+            if(week.get(i).equals(day)){
+                return week.get(i+n);
+            }
+        }
         return "";
     }
 }
