083e80cd6baf335c5c09812c6780d5fa2386e62b
Parth Bisht <bishtparth795@gmail.com>
2023-10-19 16:59:25 +0530

    Smaller neighbour element

---
+++ Smaller Neighbour Element/src/Main.java
@@ -0,0 +1,17 @@
+/*
+Given an array, find the nearest smaller element G[i] for every element A[i] in the array such that the element has
+the index smaller than i
+
+Sample Input:
+8
+39 27 11 4 24 32 32 1
+
+Sample Output:
+-1 -1 -1 -1 4 24 24 -1
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
