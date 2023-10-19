e0208c0788d989641d57bee6cb8b96113f4c9008
Parth Bisht <bishtparth795@gmail.com>
2023-10-19 16:29:52 +0530

    circular traversal

--- Circular Traversal/src/Main.java
+++ Circular Traversal/src/Main.java
@@ -12,8 +12,23 @@ Sample Output
 7 4 1 2 3 6 9 8
  */
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int [][] matrix = new int[][]{{1,2,3},{4,5,6},{7,8,9}};
+        int n = 3;
+    }
+    public static void circularTraversal(int[][] matrix, int n){
+        List<Integer> output = new ArrayList<>();
+        int left=0,right=n-1,top=0,bottom=n-1;
+        for(int i=bottom;i>=top;i--){
+            output.add(matrix[i][left]);
+        }
+        left++;
+        for(int i=left;i<right;i++){
+            
+        }
     }
 }
