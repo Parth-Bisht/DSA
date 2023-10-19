2a04b7e00e14bde6e6ab4d3c94351ca8482025dc
Parth Bisht <bishtparth795@gmail.com>
2023-10-19 15:17:05 +0530

    Circular Traversal

---
+++ Circular Traversal/src/Main.java
@@ -0,0 +1,19 @@
+/*
+In this problem, we are given a square matrix, and we have to traverse the matrix, such that each the pattern formed
+after joining the visited indexes is circular, as shown in the image given in the problem statement
+
+Sample Input
+3
+1 2 3
+4 5 6
+7 8 9
+
+Sample Output
+7 4 1 2 3 6 9 8
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
